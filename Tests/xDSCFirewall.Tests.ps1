563aaf1e3e0b931d4fef98d44ab6ecb68c496070
Anthony <anthonywaye@gmail.com>
2016-07-23 10:22:36 +1000

    Added additional tests to pester

--- Tests/xDSCFirewall.Tests.ps1
+++ Tests/xDSCFirewall.Tests.ps1
@@ -25,6 +25,32 @@ InModuleScope XDSCFirewall {
     }
   }
 
+    Describe "Checking Set-TargetResource" {
+
+    It "Disabling firewall and configuring with values" {
+      $result = Set-TargetResource -Zone $Zone -Ensure Absent -LogAllowed False -LogBlocked True -LogIgnored NotConfigured -LogMaxSizeKilobytes 4096 `
+         -DefaultInboundAction Block -DefaultOutboundAction Allow
+    }
+  }
+
+  Describe "Firewall disabled checking ensure present logic for Test-TargetResource" {
+    $Firewall.Enabled = $false
+    Mock Get-NetFirewallProfile -MockWith { $Firewall }
+
+    It "Firewall is disabled while ensure present is set so should return false" {
+      Test-TargetResource -Zone $Zone -Ensure $EnsurePresent | Should Be 'False'
+    }
+  }
+
+  Describe "Firewall disabled checking ensure absent logic for Test-TargetResource" {
+    $Firewall.Enabled = $false
+    Mock Get-NetFirewallProfile -MockWith { $Firewall }
+
+    It "Firewall is disabled while ensure absent is set so should return true" {
+      Test-TargetResource -Zone $Zone -Ensure $EnsureAbsent | Should Be 'True'
+    }
+  }
+
   Describe "Checking Set-TargetResource" {
 
     It "Enabling firewall and configuring with values" {
@@ -40,21 +66,16 @@ InModuleScope XDSCFirewall {
     It "Firewall is enabled while ensure present is set so should return true" {
       Test-TargetResource -Zone $Zone -Ensure $EnsurePresent | Should Be 'True'
     }
-    It "Firewall is enabled while ensure absent is set so should return false" {
-      Test-TargetResource -Zone $Zone -Ensure $EnsureAbsent | Should Be 'False'
-    }
   }
 
-  Describe "Firewall disabled checking ensure present logic for Test-TargetResource" {
-    $Firewall.Enabled = $false
+  Describe "Firewall enabled checking ensure absent logic for Test-TargetResource" {
+    $Firewall.Enabled = $true
     Mock Get-NetFirewallProfile -MockWith { $Firewall }
 
-    It "Firewall is disabled while ensure present is set so should return false" {
-      Test-TargetResource -Zone $Zone -Ensure $EnsurePresent | Should Be 'False'
-    }
-    It "Firewall is disabled while ensure absent is set so should return true" {
-      Test-TargetResource -Zone $Zone -Ensure $EnsureAbsent | Should Be 'True'
+    It "Firewall is enabled while ensure absent is set so should return false" {
+      Test-TargetResource -Zone $Zone -Ensure $EnsureAbsent | Should Be 'false'
     }
   }
+
 }
 
