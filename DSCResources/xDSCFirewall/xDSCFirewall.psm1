1cfc76c614d3c3fc1b5ac638270862a9916ca1dd
Anthony <anthonywaye@gmail.com>
2016-07-25 17:55:10 +1000

    Updated formatting

--- DSCResources/xDSCFirewall/xDSCFirewall.psm1
+++ DSCResources/xDSCFirewall/xDSCFirewall.psm1
@@ -4,39 +4,40 @@
   [OutputType([System.Collections.Hashtable])]
   param
   (
-    [Parameter(Mandatory = $true)][ValidateSet("Domain","Private","Public")]
+    [Parameter(Mandatory = $true)][ValidateSet('Domain','Private','Public')]
     [System.String]
     $Zone,
 
-    [Parameter(Mandatory = $true)][ValidateSet("Present","Absent")]
+    [Parameter(Mandatory = $true)][ValidateSet('Present','Absent')]
     [System.String]
     $Ensure
   )
-  $firewall = Get-NetFirewallProfile $Zone | Select-Object Enabled,LogAllowed,LogBlocked,LogIgnored,LogMaxSizeKilobytes,DefaultInboundAction,DefaultOutboundAction
+  $firewall = Get-NetFirewallProfile $Zone | Select-Object -Property Enabled, LogAllowed, LogBlocked, LogIgnored, LogMaxSizeKilobytes, DefaultInboundAction, DefaultOutboundAction
 
-  if ($firewall.Enabled -eq $false) {
+  if ($firewall.Enabled -eq $false) 
+  {
     return @{
-      Ensure = "Absent";
-      Zone = $Zone;
-      LogAllowed = $firewall.LogAllowed;
-      LogBlocked = $firewall.LogBlocked;
-      LogIgnored = $firewall.LogIgnored;
-      LogMaxSizeKilobytes = $firewall.LogMaxSizeKilobytes;
-      DefaultInboundAction = $firewall.DefaultInboundAction;
-      DefaultOutboundAction = $firewall.DefaultOutboundAction;
+      Ensure                = 'Absent'
+      Zone                  = $Zone
+      LogAllowed            = $firewall.LogAllowed
+      LogBlocked            = $firewall.LogBlocked
+      LogIgnored            = $firewall.LogIgnored
+      LogMaxSizeKilobytes   = $firewall.LogMaxSizeKilobytes
+      DefaultInboundAction  = $firewall.DefaultInboundAction
+      DefaultOutboundAction = $firewall.DefaultOutboundAction
     }
   }
   else
   {
     return @{
-      Ensure = "Present";
-      Zone = $Zone;
-      LogAllowed = $firewall.LogAllowed;
-      LogBlocked = $firewall.LogBlocked;
-      LogIgnored = $firewall.LogIgnored;
-      LogMaxSizeKilobytes = $firewall.LogMaxSizeKilobytes;
-      DefaultInboundAction = $firewall.DefaultInboundAction;
-      DefaultOutboundAction = $firewall.DefaultOutboundAction;
+      Ensure                = 'Present'
+      Zone                  = $Zone
+      LogAllowed            = $firewall.LogAllowed
+      LogBlocked            = $firewall.LogBlocked
+      LogIgnored            = $firewall.LogIgnored
+      LogMaxSizeKilobytes   = $firewall.LogMaxSizeKilobytes
+      DefaultInboundAction  = $firewall.DefaultInboundAction
+      DefaultOutboundAction = $firewall.DefaultOutboundAction
     }
   }
 }
@@ -47,65 +48,62 @@ function Set-TargetResource
   [CmdletBinding()]
   param
   (
-    [Parameter(Mandatory = $true)][ValidateSet("Domain","Private","Public")]
+    [Parameter(Mandatory = $true)][ValidateSet('Domain','Private','Public')]
     [System.String]
     $Zone,
 
-    [Parameter(Mandatory = $true)][ValidateSet("Present","Absent")]
+    [Parameter(Mandatory = $true)][ValidateSet('Present','Absent')]
     [System.String]
     $Ensure,
 
-    [Parameter()][ValidateSet("True","False","NotConfigured")]
-    [System.String]$LogBlocked = "False",
+    [Parameter()][ValidateSet('True','False','NotConfigured')]
+    [System.String]$LogBlocked = 'False',
 
-    [Parameter()][ValidateSet("True","False","NotConfigured")]
-    [System.String]$LogAllowed = "False",
+    [Parameter()][ValidateSet('True','False','NotConfigured')]
+    [System.String]$LogAllowed = 'False',
 
-    [Parameter()][ValidateSet("True","False","NotConfigured")]
-    [System.String]$LogIgnored = "NotConfigured",
+    [Parameter()][ValidateSet('True','False','NotConfigured')]
+    [System.String]$LogIgnored = 'NotConfigured',
 
     [Parameter()]
-    [System.String]$LogMaxSizeKilobytes = "4096",
+    [System.String]$LogMaxSizeKilobytes = '4096',
 
-    [Parameter()][ValidateSet("Allow","Block","NotConfigured")]
-    [System.String]$DefaultInboundAction = "NotConfigured",
+    [Parameter()][ValidateSet('Allow','Block','NotConfigured')]
+    [System.String]$DefaultInboundAction = 'NotConfigured',
 
-    [Parameter()][ValidateSet("Allow","Block","NotConfigured")]
-    [System.String]$DefaultOutboundAction = "NotConfigured"
+    [Parameter()][ValidateSet('Allow','Block','NotConfigured')]
+    [System.String]$DefaultOutboundAction = 'NotConfigured'
   )
 
   $Output = @(
-  "Enabled firewall zone $zone and configured the following
-   DefaultInboundAction: $DefaultInboundAction
-   DefaultOutboundAction: $DefaultOutboundAction
-   LogAllowed: $LogAllowed
-   LogBlocked: $LogBlocked
-   LogIgnored: $LogIgnored
-   LogMaxSizeKilobytes: $LogMaxSizeKilobytes
+    "Enabled firewall zone $Zone and configured the following
+      DefaultInboundAction: $DefaultInboundAction
+      DefaultOutboundAction: $DefaultOutboundAction
+      LogAllowed: $LogAllowed
+      LogBlocked: $LogBlocked
+      LogIgnored: $LogIgnored
+      LogMaxSizeKilobytes: $LogMaxSizeKilobytes
   ")
 
-  if ($Ensure -eq "Present")
+  if ($Ensure -eq 'Present')
   {
     Get-NetFirewallProfile $Zone | Set-NetFirewallProfile -Enabled True -LogAllowed $LogAllowed -LogBlocked $LogBlocked -LogIgnored $LogIgnored -LogMaxSizeKilobytes $LogMaxSizeKilobytes -DefaultInboundAction $DefaultInboundAction -DefaultOutboundAction $DefaultOutboundAction
-    New-EventLog -LogName "Microsoft-Windows-DSC/Operational" -Source "xDSCFirewall" -ErrorAction SilentlyContinue
-    Write-EventLog -LogName "Microsoft-Windows-DSC/Operational" -Source "xDSCFirewall" -EventId 3001 -EntryType Information -Message ($Output | Out-String)
+    New-EventLog -LogName 'Microsoft-Windows-DSC/Operational' -Source 'xDSCFirewall' -ErrorAction SilentlyContinue
+    Write-EventLog -LogName 'Microsoft-Windows-DSC/Operational' -Source 'xDSCFirewall' -EventId 3001 -EntryType Information -Message ($Output | Out-String)
   }
-  elseif ($Ensure -eq "Absent")
+  elseif ($Ensure -eq 'Absent')
   {
     Get-NetFirewallProfile $Zone | Set-NetFirewallProfile -Enabled False -LogAllowed $LogAllowed -LogBlocked $LogBlocked -LogIgnored $LogIgnored -LogMaxSizeKilobytes $LogMaxSizeKilobytes -DefaultInboundAction $DefaultInboundAction -DefaultOutboundAction $DefaultOutboundAction
-    New-EventLog -LogName "Microsoft-Windows-DSC/Operational" -Source "xDSCFirewall" -ErrorAction SilentlyContinue
-    Write-EventLog -LogName "Microsoft-Windows-DSC/Operational" -Source "xDSCFirewall" -EventId 3001 -EntryType Information -Message ($Output | Out-String)
-
+    New-EventLog -LogName 'Microsoft-Windows-DSC/Operational' -Source 'xDSCFirewall' -ErrorAction SilentlyContinue
+    Write-EventLog -LogName 'Microsoft-Windows-DSC/Operational' -Source 'xDSCFirewall' -EventId 3001 -EntryType Information -Message ($Output | Out-String)
   }
   else
   {
-    Write-Verbose "No match"
+    Write-Verbose -Message 'No match'
   }
 
   #Include this line if the resource requires a system reboot.
   #$global:DSCMachineStatus = 1
-
-
 }
 
 
@@ -115,34 +113,34 @@ function Test-TargetResource
   [OutputType([System.Boolean])]
   param
   (
-    [Parameter(Mandatory = $true)][ValidateSet("Domain","Private","Public")]
+    [Parameter(Mandatory = $true)][ValidateSet('Domain','Private','Public')]
     [System.String]
     $Zone,
 
-    [Parameter(Mandatory = $true)][ValidateSet("Present","Absent")]
+    [Parameter(Mandatory = $true)][ValidateSet('Present','Absent')]
     [System.String]
     $Ensure,
 
-    [Parameter()][ValidateSet("True","False","NotConfigured")]
-    [System.String]$LogBlocked = "False",
+    [Parameter()][ValidateSet('True','False','NotConfigured')]
+    [System.String]$LogBlocked = 'False',
 
-    [Parameter()][ValidateSet("True","False","NotConfigured")]
-    [System.String]$LogAllowed = "False",
+    [Parameter()][ValidateSet('True','False','NotConfigured')]
+    [System.String]$LogAllowed = 'False',
 
-    [Parameter()][ValidateSet("True","False","NotConfigured")]
-    [System.String]$LogIgnored = "NotConfigured",
+    [Parameter()][ValidateSet('True','False','NotConfigured')]
+    [System.String]$LogIgnored = 'NotConfigured',
 
     [Parameter()]
-    [System.String]$LogMaxSizeKilobytes = "4096",
+    [System.String]$LogMaxSizeKilobytes = '4096',
 
-    [Parameter()][ValidateSet("Allow","Block","NotConfigured")]
-    [System.String]$DefaultInboundAction = "NotConfigured",
+    [Parameter()][ValidateSet('Allow','Block','NotConfigured')]
+    [System.String]$DefaultInboundAction = 'NotConfigured',
 
-    [Parameter()][ValidateSet("Allow","Block","NotConfigured")]
-    [System.String]$DefaultOutboundAction = "NotConfigured"
+    [Parameter()][ValidateSet('Allow','Block','NotConfigured')]
+    [System.String]$DefaultOutboundAction = 'NotConfigured'
   )
 
-  $firewall = Get-NetFirewallProfile $Zone | Select-Object Enabled,LogAllowed,LogBlocked,LogIgnored,LogMaxSizeKilobytes,DefaultInboundAction,DefaultOutboundAction
+  $firewall = Get-NetFirewallProfile $Zone | Select-Object -Property Enabled, LogAllowed, LogBlocked, LogIgnored, LogMaxSizeKilobytes, DefaultInboundAction, DefaultOutboundAction
 
   if ($Ensure -eq 'Present')
   {
